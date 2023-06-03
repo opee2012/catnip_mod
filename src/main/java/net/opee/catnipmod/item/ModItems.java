e38c5b9eca8de689a8a6a4e96c1b507781ff8d07
opee2012 <jborth2010@gmail.com>
2023-06-03 10:51:04 -0400

    Made origin compatible for felines.

--- src/main/java/net/opee/catnipmod/item/ModItems.java
+++ src/main/java/net/opee/catnipmod/item/ModItems.java
@@ -1,10 +1,7 @@
 package net.opee.catnipmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.AliasedBlockItem;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -17,15 +14,7 @@ public class ModItems {
                     new FabricItemSettings().group(ModItemGroup.CATNIP)));
 
     public static final Item CATNIP = registerItem("catnip",
-            new Item(new FabricItemSettings().group(ModItemGroup.CATNIP)
-                    .food(new FoodComponent.Builder()
-                    .hunger(0)
-                    .saturationModifier(0f)
-                    .snack()
-                    .alwaysEdible()
-                    .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 130), 1.0f)
-                    .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2400), 1.0f)
-                    .build())));
+            new Item(new FabricItemSettings().group(ModItemGroup.CATNIP)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(CatnipMod.MOD_ID, name), item);
